129400f6e858b32fdc82bae1159f9705255908e0
bogdanovsa <bogdanovsa1@edu.mos.ru>
2021-07-21 22:21:05 +0300

    update

---
+++ src/components/app-header/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Logo, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import css from './index.module.css';
+
+function AppHeader() {
+    const [current, setCurrent] = React.useState('one')
+    return (
+        <div className={css.app_header}>
+
+            <div>
+                <BurgerIcon type="primary" />
+Конструктор
+
+            <ListIcon type="primary" />
+Лента Заказов
+            </div>
+
+            <Logo/>
+            <div>
+            <ProfileIcon type="primary" />
+Личный кабинет
+            </div>
+        </div>
+    );
+}
+
+export default AppHeader;
