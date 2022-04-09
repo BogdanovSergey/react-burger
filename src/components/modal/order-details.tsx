ecbbae1b9b23b43fa783f28d95bd62d591968531
bogdanovsa <bogdanovsa1@edu.mos.ru>
2022-04-09 23:03:01 +0300

    sprint-5/step-1 [fix]

--- src/components/modal/order-details.tsx
+++ src/components/modal/order-details.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import css from './modal.module.css';
 import imgDone from '../../images/done.svg';
 import { useSelector } from '../../hooks/hooks';
@@ -27,15 +27,4 @@ export const OrderDetails = () => {
 			</div>
 		)
 	}
-	/*return(
-		<div className={css.order_details}>
-			<p className="text text_type_digits-large">{orderObj.number}</p>
-			<p className={css.identificator_size + " text text_type_main-medium"}>идентификатор заказа</p>
-			<img src={imgDone} alt={"done"}/>
-			<span className={css.bottom_text + " pt-7 text text_type_main-small"}>
-				<p>Ваш заказ начали готовить</p>
-				<p>Дождитесь готовности на орбитальной станции</p>
-			</span>
-		</div>
-	);*/
 }
