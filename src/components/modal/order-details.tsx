15612366805b99c43b5c355d43ac8d29d4c56bd8
bogdanovsa <bogdanovsa1@edu.mos.ru>
2022-04-20 00:14:35 +0300

    fixes

--- src/components/modal/order-details.tsx
+++ src/components/modal/order-details.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import css from './modal.module.css';
-import imgDone from '../../images/done.png';
 import { useSelector } from '../../hooks/hooks';
 
 export const OrderDetails = () => {
@@ -11,7 +10,7 @@ export const OrderDetails = () => {
 			<div className={css.order_details}>
 				<p className="text text_type_digits-large">{orderObj.number}</p>
 				<p className={css.identificator_size + " text text_type_main-medium"}>идентификатор заказа</p>
-				<img src={imgDone} alt={"done"}/>
+				<div className={css.done_btn}></div>
 				<span className={css.bottom_text + " pt-7 text text_type_main-small"}>
 						<p>Ваш заказ начали готовить</p>
 						<p>Дождитесь готовности на орбитальной станции</p>
